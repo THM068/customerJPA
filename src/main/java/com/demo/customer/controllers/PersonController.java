0d6c2c7671357f76ab80dc69259955b104ebb23e
tm1c14 <tm1c14@soton.ac.uk>
2015-06-09 17:06:32 +0100

    Added support for hateos

--- src/main/java/com/demo/customer/controllers/PersonController.java
+++ src/main/java/com/demo/customer/controllers/PersonController.java
@@ -3,7 +3,13 @@ package com.demo.customer.controllers;
 import com.demo.customer.domain.Person;
 import com.demo.customer.services.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,9 +33,18 @@ public class PersonController {
     }
 
     @RequestMapping(value = "list", method = RequestMethod.GET, produces = "application/json")
-    public Iterable<Person> getList(Pageable pageable) {
+    public Iterable<Person> getList(Pageable pageable) { //?sort=name,desc&size=4&page=1
         Iterable<Person> iterable = personRepository.findAll(pageable);
         return iterable;
+    }
+
+    //Hypermedia support for Pageables
+    @RequestMapping(value = "pages", method = RequestMethod.GET, produces = "application/json")
+    HttpEntity<PagedResources<Person>> persons(Pageable pageable, PagedResourcesAssembler assembler) {
+
+        Page<Person> persons = personRepository.findAll(pageable);
+        ResponseEntity entity = new ResponseEntity(assembler.toResource(persons), HttpStatus.OK);
+        return entity;
     }
 
 
