14b2e37855a441f8b1e7f05d475b9a634ddedb5f
tm1c14 <tm1c14@soton.ac.uk>
2015-06-10 12:32:27 +0100

    requestbody

--- src/main/java/com/demo/customer/controllers/PersonController.java
+++ src/main/java/com/demo/customer/controllers/PersonController.java
@@ -10,11 +10,12 @@ import org.springframework.hateoas.PagedResources;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 
 /**
@@ -27,24 +28,37 @@ public class PersonController {
     @Autowired
     private PersonRepository personRepository;
 
+    @ResponseStatus(HttpStatus.OK)
     @RequestMapping(value = "{id}", method = RequestMethod.GET, produces = "application/json")
     public Person getPerson(@PathVariable("id")Person person) {
         return person;
     }
 
-    @RequestMapping(value = "list", method = RequestMethod.GET, produces = "application/json")
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = "list", method = RequestMethod.GET, headers = {"Accept=text/xml, application/json"})
     public Iterable<Person> getList(Pageable pageable) { //?sort=name,desc&size=4&page=1
         Iterable<Person> iterable = personRepository.findAll(pageable);
         return iterable;
     }
 
     //Hypermedia support for Pageables
-    @RequestMapping(value = "pages", method = RequestMethod.GET, produces = "application/json")
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = "pages", method = RequestMethod.GET, headers = {"Accept=application/json"})
     HttpEntity<PagedResources<Person>> persons(Pageable pageable, PagedResourcesAssembler assembler) {
 
         Page<Person> persons = personRepository.findAll(pageable);
         ResponseEntity entity = new ResponseEntity(assembler.toResource(persons), HttpStatus.OK);
         return entity;
+    }
+
+    //add a person
+    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping(method = RequestMethod.PUT, headers = {"Accept=application/json", "Content-type=application/json"} , consumes = "application/json")
+    public Person create(@RequestBody Person person,  HttpServletResponse response)  {
+        Person p = personRepository.save(person);
+        response.setHeader("Location", "/person/"+ p.getId());
+
+        return p;
     }
 
 
